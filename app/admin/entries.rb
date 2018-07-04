786d86f083502747062a2ef8dae26e5b4c542ae2
James Kelsey <jamesmskelsey@gmail.com>
2018-07-03 21:10:40 -0700

    add sections to activeadmin

---
+++ app/admin/entries.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Entry do
+  permit_params :pub_number, :copy_number, :work_center, :pub_type, :classification, :title, :issue_date, :last_audit_date
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+end
